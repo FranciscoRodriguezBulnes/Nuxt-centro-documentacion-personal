207d0f8b9b5da43dd0337afdb280f89c39743a70
Francisco Rodríguez Bulnes <158209260+FranciscoRodriguezBulnes@users.noreply.github.com>
2026-03-27 06:50:45 +0100

    Generar HTML estático correctamente

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+
+  ssr: true, // o simplemente no poner nada, pero lo dejamos explícito
+
   nitro: {
     preset: "node-server",
+    prerender: {
+      routes: ["/"],
+    },
+  },
+
+  routeRules: {
+    "/": { prerender: true },
   },
 });
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Centro de documentación personal</h1>
+</template>
